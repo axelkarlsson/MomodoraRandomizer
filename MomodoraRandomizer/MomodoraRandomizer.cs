4ccd2d2a4b5ab1edf718b3f75decc1080699a4a3
Axel <karlssonaxel96@gmail.com>
2022-01-20 02:59:12 +0100

    Base for  Randomizer Component
    Set up a visual studio project as a base for the randomizer

---
+++ MomodoraRandomizer/MomodoraRandomizer.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Windows.Forms;
+using LiveSplit.ComponentUtil;
+using LiveSplit.Model;
+
+namespace LiveSplit.UI.Components
+{
+    public class MomodoraRandomizer : IComponent
+    {
+        private SimpleLabel RandomizerLabel;
+        private Process gameProc = null;
+        private Random randomGenerator = null;
+        private int seed = 0;
+
+        public MomodoraRandomizer(LiveSplitState state)
+        {
+            RandomizerLabel = new SimpleLabel("Randomizer Go!");
+        }
+
+        private void onStart(object sender, EventArgs e)
+        {
+            if (VerifyProcessRunning())
+            {
+                //If set seed ->
+                randomGenerator = new Random(0);
+                //else, random seed
+                //Thought it would be nice to do it this way so that the seed can be stored and displayed so that multiple people can run the same seed etc
+                randomGenerator = new Random();
+                seed = randomGenerator.Next();
+                randomGenerator = new Random(seed);
+
+
+                //Setup magic here!
+            }
+        }
+
+        public string ComponentName => "Momodora Randomizer";
+
+        public float HorizontalWidth {get; set;}
+
+        public float MinimumHeight => 10;
+
+        public float VerticalHeight { get; set; }
+
+        public float MinimumWidth => 200;
+
+        public float PaddingTop => 1;
+
+        public float PaddingBottom => 1;
+
+        public float PaddingLeft => 1;
+
+        public float PaddingRight => 1;
+
+        public IDictionary<string, Action> ContextMenuControls => null;
+
+        public void Dispose()
+        {
+        }
+
+        public void DrawHorizontal(System.Drawing.Graphics g, LiveSplitState state, float height, System.Drawing.Region clipRegion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DrawVertical(System.Drawing.Graphics g, LiveSplitState state, float width, System.Drawing.Region clipRegion)
+        {
+            var textHeight = g.MeasureString("A", state.LayoutSettings.TextFont).Height;
+            VerticalHeight = textHeight * 1.5f;
+
+            RandomizerLabel.SetActualWidth(g);
+            RandomizerLabel.Width = RandomizerLabel.ActualWidth;
+            RandomizerLabel.Height = VerticalHeight;
+            RandomizerLabel.X = width - RandomizerLabel.ActualWidth;
+            RandomizerLabel.Y = textHeight * 0.42f - (3.5f * textHeight);
+
+            RandomizerLabel.Draw(g);
+        }
+
+        public XmlNode GetSettings(XmlDocument document)
+        {
+            throw new NotImplementedException();
+            //return settingsControl.GetSettings(document)
+        }
+
+        public System.Windows.Forms.Control GetSettingsControl(LayoutMode mode)
+        {
+            //return settingsControl;
+            throw new NotImplementedException();
+        }
+
+        public void SetSettings(XmlNode settings)
+        {
+            //settingsControl.SetSettings(settings);
+            throw new NotImplementedException();
+        }
+
+        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+            //do update stuff here!
+
+            if (invalidator != null)
+            {
+                invalidator.Invalidate(0, 0, width, height);
+            }
+        }
+
+        private bool VerifyProcessRunning()
+        {
+            if (gameProc != null && !gameProc.HasExited)
+            {
+                return true;
+            }
+            Process[] game = Process.GetProcessesByName("MomodoraRUtM");
+            if (game.Length > 0)
+            {
+                switch (game[0].MainModule.ModuleMemorySize)
+                {
+                    case 39690240:
+                        //version 1.05b
+                        gameProc = game[0];
+                        RandomizerLabel.Text = "Randomizer Ready!";
+                        return true;
+                    default:
+                        RandomizerLabel.Text = "Unsupported game version for randomizer";
+                        break;
+
+                }   
+            }
+            return false;
+        }
+    }
+}
